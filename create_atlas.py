845280807f865349f6bc85422646cfbe054aae15
GrishaYak <gishayk@yandex.ru>
2025-10-25 00:19:22 +0500

    improved working with directories and fixed saving with transparent bg

--- create_atlas.py
+++ create_atlas.py
@@ -1,3 +1,4 @@
+import os
 from tkinter import filedialog, Tk, Button
 from PIL import Image
 from os import path, listdir
@@ -53,13 +54,22 @@ def open_files():
 def main(file_names):
     if not file_names:
         return
+    global out_dir
+    if out_dir is None:
+        least = min(file_names, key=len).replace('\\', '/')
+        out_dir = least[:least.rfind('/')]
+        last_dir = out_dir[out_dir.rfind('/') + 1:]
+        if last_dir in {'images', 'ims'}:
+            out_dir = out_dir[:-len(last_dir) - 1]
+        out_dir += '/res'
+
     images = []
     height = 0
     width = 0
     for file in file_names:
         with Image.open(file) as img:
             img.load()
-            images.append(img)
+            images.append(img.convert('RGBA'))
             width += img.size[0]
             height = max(height, img.size[1])
 
@@ -73,17 +83,19 @@ def main(file_names):
         w, h = img.size[:2]
         ET.SubElement(root, "Region", attrib={"name": name, "x": str(x), "y": str(y)
             , "width": str(w), "height": str(h)})
-        arr = np.ones((h, w, 4)) * 255
-        arr[:, :, :-1] = np.array(img)
-        res[y:y + h, x:x + w] = arr
+        res[y:y + h, x:x + w] = img
         x += w
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
     tree = ET.ElementTree(root)
-    tree.write("res/atlas.xml", encoding="utf-8", xml_declaration=True)
+    tree.write(f"{out_dir}/atlas.xml", encoding="utf-8", xml_declaration=True)
     atlas = Image.fromarray(res)
-    atlas.save("res/atlas.png")
+    atlas.save(f"{out_dir}/atlas.png")
 
 if __name__ == '__main__':
-    mydir = path.dirname(path.realpath(__file__))
+    # mydir = path.dirname(path.realpath(__file__))
+    mydir = 'D:/cs/atlasManager'
+    out_dir = None
     w = Tk()
     image_formates = '.jpg .png .gif .webp'.split(' ')
     get_file_names()
